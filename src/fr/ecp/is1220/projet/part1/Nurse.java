377709bae1fde7aeefed252dfd21fbece11b44a5
benoitcharmettant <benoit.charmettant@student.ecp.fr>
2017-11-08 13:00:19 +0100

    Chagement version pas compliquée de nurse State

--- src/fr/ecp/is1220/projet/part1/Nurse.java
+++ src/fr/ecp/is1220/projet/part1/Nurse.java
@@ -4,34 +4,17 @@ public class Nurse extends HumanRessources{
 	NurseState state;
 	/**
 	 * Returns the state of the nurse
-	 * @return int number :  0 if the nurse is ONDUTY, 1 if the nurse is OFFDUTY, 2 if the nurse is OCCUPIED
+	 * @return NurseState (Enum)
 	 */
-	public int getState(){
-		if(this.state == NurseState.OCCUPIED){
-			return 2;
-		}else if(this.state == NurseState.OFFDUTY){
-			return 1;
-		}
-		return 0;
+	public NurseState getState(){
+		return this.state;
 	}
 	/**
 	 * 
-	 * @param choice
-	 * 
-	 *  Set the state of the nurse by an int number : 
-	 *  
-	 *  1 means OFFDUTY
-	 *  2 means OCCUPIED 
-	 *  0 (or any other number) means ONDUTY
+	 * @param choice (NurseState Enum)
 	 */
-	public void setState(int choice){
-		if (choice == 1){
-			this.state = NurseState.OFFDUTY;
-		}else if(choice == 2){
-			this.state = NurseState.OCCUPIED;
-		}else{
-			this.state = NurseState.ONDUTY;
-		}
+	public void setState(NurseState choice){
+		this.state = choice;
 		
 	}
 	/**
@@ -42,16 +25,13 @@ public class Nurse extends HumanRessources{
 	 * 
 	 *  A nurse also has a state (OFFDUTY, ONDUTY, OCCUPIED), its state is set ONDUTY by default 
 	 *  
-	 *  it can also be setted at the declaration of the nurse by an int number : 
-	 *  1 means OFFDUTY
-	 *  2 means OCCUPIED 
-	 *  0 (or any other number) means ONDUTY
+	 *  it can also be setted at the declaration, by adding a NurseState Parameter 
 	 */
 	public Nurse(String name, String surname) {
 		super(name, surname);
 		this.state = NurseState.ONDUTY;
 	}
-	public Nurse(String name, String surname, int choice) {
+	public Nurse(String name, String surname, NurseState choice) {
 		super(name, surname);
 		this.setState(choice);
 	}
