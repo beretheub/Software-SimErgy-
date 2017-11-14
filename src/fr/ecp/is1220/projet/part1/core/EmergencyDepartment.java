c86838c405068ef6f3ec7307e5d3f24a7806298f
benoitcharmettant <benoit.charmettant@student.ecp.fr>
2017-11-14 01:04:36 +0100

    triage

--- src/fr/ecp/is1220/projet/part1/core/EmergencyDepartment.java
+++ src/fr/ecp/is1220/projet/part1/core/EmergencyDepartment.java
@@ -69,14 +69,33 @@ public class EmergencyDepartment {
 		}
 	}
 	
-	private boolean isNurseAvailable() {
+	/**
+	 * Return the first free resource of the type you choose with your arguent (String) btw Nurse, Physician, Transporter
+	 * @param type
+	 * @return 
+	 */
+	private HumanResources returnFreeHumanResource(String type) {
 		// Je ne fais pas le truc le plus optimisé
 		for (Resources resources : edResources) {
-	
-			
-			
+			if (resources.getType() == type && type.equalsIgnoreCase("nurse")){
+				Nurse resources1 = (Nurse) resources;
+				if (resources1.getState() == NurseState.ONDUTY){
+					return (HumanResources) resources;
+					}
+				}else if (resources.getType() == type && type.equalsIgnoreCase("physician")){
+					Physician resources1 = (Physician) resources;
+					if (resources1.getState() == PhysicianState.ONDUTY){
+						return (HumanResources) resources;
+					}
+				}else if (resources.getType() == type && type.equalsIgnoreCase("transporter")){
+					Transporter resources1 = (Transporter) resources;
+					if (resources1.getState() == TransporterState.ONDUTY){
+						return (HumanResources) resources;
+					}
+				}else{
+					return null;
+				}
 		}
-		return false;
 	}
 	
 	// -------------------------------------------------
